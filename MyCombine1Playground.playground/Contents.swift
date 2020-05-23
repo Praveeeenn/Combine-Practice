847c6e135a9fe27aeb0b7c3857bb10265f9f7082
Praveen <praveenislive@gmail.com>
2020-05-23 19:15:58 +0530

    Custom Subscriber

---
+++ MyCombine1Playground.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+import Combine
+
+//Custome Subscriber
+
+
+// 1. Create a publisher of integers vis the range's publihser property.
+let publisher = (1...6).publisher
+
+
+//2. Define Custom Subscriber , IntSubscriber
+final class IntSubscriber: Subscriber {
+    
+    //3. Implementing the type alias to specify that his subscriber can recieve Ingteger  and will never receeve
+    typealias Input = Int
+    typealias Failure = Never
+    
+    //4.
+    func receive(subscription: Subscription) {
+        print("Receive Subscription !!")
+        subscription.request(.max(3))
+    }
+    
+    //5.
+    func receive(_ input: Int) -> Subscribers.Demand {
+        print("Receive value : - ", input)
+        return .none
+    }
+    
+    //6.
+    func receive(completion: Subscribers.Completion<Never>) {
+        print("Receive Completion", completion)
+    }
+    
+}
